68f43f47bb1a24582dab24f14024b7072b346157
김호섭 <hskim@beaverworksinc.com>
2024-11-14 18:04:02 +0900

    svg parsing ...ing

---
+++ BootStrapSvgIconToShapePath/Program.cs
@@ -0,0 +1,41 @@
+﻿using System.Xml;
+using System.Xml.Linq;
+
+namespace BootStrapSvgIconToShapePath
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Input path for bootstrap svg files : ");
+            string? path = Console.ReadLine();
+
+            if (path != null)
+            {
+                string rdfile = @".\BootStrapIcons.xmal";
+                string[] files = Directory.GetFiles(path, "*.svg");
+
+                
+                foreach (string file in files)
+                {
+                    Console.WriteLine(file);
+
+                    XElement svg = XElement.Load(file);
+
+                    svg.Elements().ToList().ForEach(e => { 
+                        
+                        if (e.Name == "path")
+                        {
+                            string data = e.Attribute("d")!.Value;
+
+
+                        }
+                    });
+
+                }
+                
+            }
+            //Console.WriteLine("Hello, World!");
+        }
+    }
+}
